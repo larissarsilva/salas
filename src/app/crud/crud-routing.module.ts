58ea17c440daf6b7a499c45d0ff4a30ab7202c66
Larissa Silva <lrpas@ecomp.poli.br>
2023-01-23 16:20:42 -0300

    sidenav first config

--- src/app/crud/crud-routing.module.ts
+++ src/app/crud/crud-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CrudComponent } from './crud.component';
+import { ProfessorComponent } from './professor/professor.component';
+import { StudentComponent } from './student/student.component';
 
 const routes: Routes = [
-  {path: '', component: CrudComponent}
+  {path: '', component: CrudComponent, children: [
+    {
+      path: '',
+      redirectTo: 'professor',
+      pathMatch: 'full'
+    },
+    {path: 'professor', component: ProfessorComponent},
+    {path: 'estudante', component: StudentComponent}
+  ]}
 ];
 
 @NgModule({
--- src/app/crud/crud.module.ts
+++ src/app/crud/crud.module.ts
@@ -4,13 +4,25 @@ import { CrudComponent } from './crud.component';
 import { CrudRoutingModule } from './crud-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { CrudService } from './crud.service';
+import { ProfessorComponent } from './professor/professor.component';
+import { StudentComponent } from './student/student.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [CrudComponent],
+  declarations: [CrudComponent, ProfessorComponent, StudentComponent],
   imports: [
     CommonModule,
     CrudRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [CrudService],
 })

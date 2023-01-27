8bc074470d72cf4ce19dae6a0d2d4d1bfd7b79e7
Larissa Silva <lrpas@ecomp.poli.br>
2023-01-26 23:45:39 -0300

    tabela de cursos e horario

--- src/app/crud/course/course.component.ts
+++ src/app/crud/course/course.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+  styleUrls: ['./course.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
-export class CourseComponent {
+export class CourseComponent implements OnInit {
+response = [
+    {
+      "name": "Engenharia da Computação",
+      "shift": 1,
+      "subjects": [
+        {
+          "name": "calculo numerico",
+          "code": "GHBP9765",
+          "hours": 15,
+          "group": "st",
+          "courses": null,
+          "professors": [],
+          "id": 1
+        },
+        {
+          "name": "LPI",
+          "code": "GHBP9765",
+          "hours": 30,
+          "group": "st",
+          "courses": null,
+          "professors": [],
+          "id": 1
+        }
+      ],
+      "id": 1
+    },
+    {
+      "name": "Engenharia Civil",
+      "shift": 0,
+      "subjects": [],
+      "id": 3
+    }
+  ];
+
+  columnsToDisplay = ['name', 'shift'];
+  columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
+  expandedElement = this.response;
+
+
+ dataSource = this.response;
+
+  ngOnInit() { 
+  }
 
 }
--- src/app/crud/room/room.component.ts
+++ src/app/crud/room/room.component.ts
@@ -1,10 +1,190 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-room',
   templateUrl: './room.component.html',
   styleUrls: ['./room.component.css']
 })
-export class RoomComponent {
+export class RoomComponent  implements OnInit {
+
+  displayedColumns: string[] = ['name', 'block', 'isAcessible', 
+  'hasAirConditioner', 'hasFan', 'hasProjector', 'capacity',
+  'key', 'available', 'note', 'action'];
+
+  ELEMENT_DATA = [];
+  
+  response = [{
+        "id": 0,
+        "name": "K 09",
+        "block": 0,
+        "isAcessible": true,
+        "hasAirConditioner": true,
+        "hasFan": true,
+        "hasProjector": true,
+        "capacity": 30,
+        "keyId": 0,
+        "key": {
+          "id": 0,
+          "uuid": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+        },
+        "available": true,
+        "note": "Esta sala não possue cadeira para canhoto"
+    },
+    {
+      "id": 1,
+      "name": "LIP 07",
+      "block": 0,
+      "isAcessible": false,
+      "hasAirConditioner": true,
+      "hasFan": true,
+      "hasProjector": false,
+      "capacity": 45,
+      "keyId": 0,
+      "key": {
+        "id": 0,
+        "uuid": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+      },
+      "available": true,
+      "note": "Esta sala não possue nenhuma restrição"
+  },
+  {
+    "id": 0,
+    "name": "LIP 07",
+    "block": 0,
+    "isAcessible": false,
+    "hasAirConditioner": true,
+    "hasFan": true,
+    "hasProjector": false,
+    "capacity": 45,
+    "keyId": 0,
+    "key": {
+      "id": 0,
+      "uuid": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    },
+    "available": true,
+    "note": "Esta sala não possue nenhuma restrição"
+},
+{
+  "id": 0,
+  "name": "LIP 07",
+  "block": 0,
+  "isAcessible": false,
+  "hasAirConditioner": true,
+  "hasFan": true,
+  "hasProjector": false,
+  "capacity": 45,
+  "keyId": 0,
+  "key": {
+    "id": 0,
+    "uuid": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+  },
+  "available": true,
+  "note": "Esta sala não possue nenhuma restrição"
+},
+{
+  "id": 0,
+  "name": "LIP 07",
+  "block": 0,
+  "isAcessible": false,
+  "hasAirConditioner": true,
+  "hasFan": true,
+  "hasProjector": false,
+  "capacity": 45,
+  "keyId": 0,
+  "key": {
+    "id": 0,
+    "uuid": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+  },
+  "available": true,
+  "note": "Esta sala não possue nenhuma restrição"
+},
+{
+  "id": 0,
+  "name": "LIP 07",
+  "block": 0,
+  "isAcessible": false,
+  "hasAirConditioner": true,
+  "hasFan": true,
+  "hasProjector": false,
+  "capacity": 45,
+  "keyId": 0,
+  "key": {
+    "id": 0,
+    "uuid": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+  },
+  "available": true,
+  "note": "Esta sala não possue nenhuma restrição"
+},{
+  "id": 0,
+  "name": "LIP 07",
+  "block": 0,
+  "isAcessible": false,
+  "hasAirConditioner": true,
+  "hasFan": true,
+  "hasProjector": false,
+  "capacity": 45,
+  "keyId": 0,
+  "key": {
+    "id": 0,
+    "uuid": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+  },
+  "available": true,
+  "note": "Esta sala não possue nenhuma restrição"
+},{
+  "id": 0,
+  "name": "LIP 07",
+  "block": 0,
+  "isAcessible": false,
+  "hasAirConditioner": true,
+  "hasFan": true,
+  "hasProjector": false,
+  "capacity": 45,
+  "keyId": 0,
+  "key": {
+    "id": 0,
+    "uuid": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+  },
+  "available": true,
+  "note": "Esta sala não possue nenhuma restrição"
+},{
+  "id": 0,
+  "name": "LIP 07",
+  "block": 0,
+  "isAcessible": false,
+  "hasAirConditioner": true,
+  "hasFan": true,
+  "hasProjector": false,
+  "capacity": 45,
+  "keyId": 0,
+  "key": {
+    "id": 0,
+    "uuid": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+  },
+  "available": true,
+  "note": "Esta sala não possue nenhuma restrição"
+},{
+  "id": 0,
+  "name": "LIP 07",
+  "block": 0,
+  "isAcessible": false,
+  "hasAirConditioner": true,
+  "hasFan": true,
+  "hasProjector": false,
+  "capacity": 45,
+  "keyId": 0,
+  "key": {
+    "id": 0,
+    "uuid": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+  },
+  "available": true,
+  "note": "Esta sala não possue nenhuma restrição"
+}
+
+];
+    dataSource = this.response;
+ 
+    ngOnInit() { 
+  }
+
 
 }
